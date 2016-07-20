f5512d9d5b2c8b827dfc3c81a3fcd1fc703fc89f
Devdatta <devdatta.godbole777@gmail.com>
2016-07-20 12:37:00 +0530

    Addition of buttons

--- app/src/main/java/kz/passqr/MainActivity.java
+++ app/src/main/java/kz/passqr/MainActivity.java
@@ -1,13 +1,28 @@
 package kz.passqr;
 
+import android.app.Activity;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+   public void clickFuncShowQR(View view){
+       Toast.makeText(MainActivity.this, "ShowQR Clicked", Toast.LENGTH_SHORT).show();
+   }
+
+    public void clickFuncExit(View view){
+        Toast.makeText(MainActivity.this,"Exit clicked", Toast.LENGTH_SHORT).show();
+        System.exit(0);
+
     }
 }
